6cff794819b6c6338e83329ee60955db4ebbadac
FHao233 <610284360@qq.com>
2022-11-21 15:44:51 +0800

    初始化项目

---
+++ src/main/java/com/fhao/mini_generator/MiniGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fhao.mini_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniGeneratorApplication.class, args);
+    }
+
+}
